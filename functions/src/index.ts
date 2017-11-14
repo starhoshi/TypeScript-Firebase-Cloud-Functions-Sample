c5d572a78a68d993053ef20f3a0f0ae028885a4e
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2017-11-15 02:42:58 +0900

    add index.ts

---
+++ functions/src/index.ts
@@ -0,0 +1,7 @@
+import * as functions from 'firebase-functions'
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!\n\n")
+})
