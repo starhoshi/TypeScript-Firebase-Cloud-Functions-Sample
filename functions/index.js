a3e51fb436d541bf152a6b66c46287895348afaa
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2017-11-16 01:35:13 +0900

    remove express

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld2 = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!\n\n");
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,6 @@ import * as functions from 'firebase-functions'
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld2 = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!\n\n")
 })
