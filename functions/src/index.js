2fda069a8a85dd0a1799a06345b31fd25e3e51b9
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2017-11-17 01:30:37 +0900

    build

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var functions = require("firebase-functions");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-exports.helloWorld2 = functions.https.onRequest(function (request, response) {
+exports.helloWorld = functions.https.onRequest(function (request, response) {
     response.send("Hello from Firebase!\n\n");
 });
